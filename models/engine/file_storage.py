da59cf7076a6835e0a16c034968117bd9591ea5b
Fortunatekio <fortunatemugendi8@gmail.com>
2023-10-12 07:18:35 +0300

    file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""storage of file"""
+import json
+
+class FileStorage:
+    """saves and restores objects from a file"""
+    def __init__(self):
+        """the path to the json file whre objects will be stored"""
